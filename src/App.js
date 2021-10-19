9fa17ce477abd3837133726f9cf2773e8e6be688
Ajayi Peter <peterdgreat2016@gmail.com>
2021-10-19 20:22:18 +0200

    Bootstrap Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App" />
+    <div>
+      <Calculator />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
