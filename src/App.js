b5f92d52a1175c9353f435eaa389586d2769c2d7
Ajayi Peter <peterdgreat2016@gmail.com>
2021-10-20 10:50:12 +0200

    Update App.js
    - Changed the component to a class based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
 import './App.css';
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="main-container col-8 d-flex flex-column justify-content-center">
-      <Calculator />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="main-container col-8 d-flex flex-column justify-content-center">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
-export default App;
