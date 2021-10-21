6496d13e21083293a28329003549cef2945ab999
Ajayi Peter <peterdgreat2016@gmail.com>
2021-10-21 18:06:42 +0200

    Refactored App.js to use functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 /* eslint-disable react/prefer-stateless-function */
 import './App.css';
-import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="main-container col-8 d-flex flex-column justify-content-center">
-        <Calculator />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="main-container col-8 d-flex flex-column justify-content-center">
+      <Calculator />
+    </div>
+  );
 }
