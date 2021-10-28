659f4f71f6eb354f8a42b748e11e692d09dd8445
Ajayi Peter <peterdgreat2016@gmail.com>
2021-10-28 21:43:16 +0200

    All Components snapshot written

--- src/test/page.test.js
+++ src/test/page.test.js
@@ -5,6 +5,7 @@ import Home from '../components/Home';
 import Quote from '../components/Quote';
 import Calculator from '../components/Calculator';
 import NavBar from '../components/NavBar';
+import Button from '../components/Button';
 import App from '../App';
 
 it('renders without crashing', () => {
@@ -29,6 +30,48 @@ describe('Testing Container page with jest snapshot', () => {
   it('renders correctly', () => {
     const tree = renderer
       .create(<App />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // test Home page with jest snapshot
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Home />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  // test Quote page with jest snapshot
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Quote />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  // test Calculator page with jest snapshot
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Calculator />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // test NavBar page with jest snapshot
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Router>
+          <NavBar />
+        </Router>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // test Button page with jest snapshot
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Button />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
